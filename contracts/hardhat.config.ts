5ed0eb955c5fd30ecccd1da1a83b6679a5c4d921
zkfriendly <zkfriendly@proton.me>
2025-01-05 13:47:49 +0100

    refactor: remove unused bundler services and task loading
    - Remove task auto-loading functionality from hardhat config
    - Remove bundler-unsafe and contracts services from docker-compose

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,14 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import * as dotenv from "dotenv";
-import fs from "fs";
 dotenv.config();
-
-// load tasks from tasks folder
-const tasks = fs.readdirSync("./tasks").filter((file) => file.endsWith(".ts"));
-tasks.forEach((task) => {
-  require(`./tasks/${task}`);
-});
 
 const { NODE_URL } = process.env;
 
